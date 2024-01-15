4bf35c635104095dd87bb4ecf4592d52c1c29797
mainak-wq <mainakdeb1998@gmail.com>
2024-01-15 14:32:56 +0530

    mainak's commit

---
+++ class_obj.py
@@ -0,0 +1,16 @@
+# a=32
+# b=36.3
+# c=56+35j
+# d=False
+# print(type(a),type(b),type(c),type(d))
+
+
+# class Msit:
+#     def main():
+#         xyz=int(input())
+#         abc=int(xyz)
+#         print(type(abc))
+# Msit.main()
+
+
+
---
+++ databaseMYSQL.py
@@ -0,0 +1,70 @@
+# ##check database connection
+
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password=""
+# )
+
+# print(mydb)
+
+
+
+# # ##Create database in python
+
+
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password=""
+# )
+# mycursor = mydb.cursor()
+# mycursor.execute("CREATE DATABASE msit_bca_sec_b")
+
+
+
+
+
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="",
+#   database="msit_bca_sec_b"
+# )
+# mycursor = mydb.cursor()
+# mycursor.execute("CREATE TABLE subjects (digital_electronics VARCHAR(255),soft_engg VARCHAR(255),cyber_sec VARCHAR(255), c_data_structure VARCHAR(255))")
+
+
+
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="msit_bca_sec_b"
+)
+mycursor = mydb.cursor()
+mycursor.execute("DROP TABLE students")
+
+
+
+# mycursor.execute("CREATE DATABASE python")
+
+
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="yourusername",
+#   password="yourpassword",
+#   database="mydatabase"
+# )
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+
